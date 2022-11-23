87d23093a05a3c3ccb0241fcd0f30faa1422db09
Paul ARNAUD <paularnaud2@gmail.com>
2022-11-23 09:34:26 +0100

    setup exclude tests

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-pkg_list = find_packages(exclude=['tests'])
+pkg_list = find_packages(exclude=['tests*'])
 setup(
     packages=pkg_list,
 )
