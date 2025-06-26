90e831853cdd12920bc3827d3a13103979709c63
Paul ARNAUD <paularnaud2@gmail.com>
2025-06-26 18:02:01 +0200

    fixed push

--- scripts/PyPi_push.ps1
+++ scripts/PyPi_push.ps1
@@ -1,6 +1,9 @@
 if (Test-Path "dist") {
   Remove-Item "dist" -Recurse
 }
+if (Test-Path "build") {
+  Remove-Item "build" -Recurse
+}
 python -m build --wheel
 twine check dist/*
 twine upload dist/*
