695622a0930e8ca77feac0a70ff04de52f4b582d
Paul ARNAUD <paularnaud2@gmail.com>
2026-05-12 13:22:00 +0200

    v1.3.5

--- parutils/changelog.py
+++ parutils/changelog.py
@@ -1,5 +1,9 @@
-__VERSION__ = '1.3.4'
-# Fixed bug empty_log_every_buffer
+__VERSION__ = '1.3.5'
+# No line break on empty_log_every_buffer if s void
+# empty_log_every_buffer on Logger.close()
+
+# __VERSION__ = '1.3.4'
+# # Fixed bug empty_log_every_buffer
 
 # __VERSION__ = '1.3.3'
 # # No more unwanted line break in log buffer
